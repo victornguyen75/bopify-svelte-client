d89967880a2276fec59004022cb85079140ee654
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-08-19 16:22:54 -0500

    feat(App.svelte, Login.svelte): add login page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
+	import Login from "./pages/Login/Login.svelte";
+	import "bootstrap/dist/css/bootstrap.min.css";
 </script>
-
-<main>
-	<h1>Hello World!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
 </style>
+
+<Login />
