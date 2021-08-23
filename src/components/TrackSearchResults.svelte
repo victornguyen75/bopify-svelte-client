93e89bd2167fd562ff73869621d4c7f483a73a43
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-08-23 18:36:50 -0500

    style(TrackSearchResults.svelte): move styles into classes

--- src/components/TrackSearchResults.svelte
+++ src/components/TrackSearchResults.svelte
@@ -4,13 +4,25 @@
 </script>
 
 <div 
-  class="d-flex m-2 align-items-center" 
-  style="cursor: pointer"
+  class="d-flex m-2 align-items-center pointer"
   on:click={() => handleSelectedTrack(track)}
   >
-  <img alt={`${track.title} album image`} src={track.albumUrl} style="height: 64px; width: 64px;" />
-  <div style="margin-left: 1rem">
+  <img alt={`${track.title} album image`} src={track.albumUrl} class="album" />
+  <div class="ml">
     <div>{track.title}</div>
     <div class="text-muted">{track.artist}</div>
   </div>
 </div>
+
+<style>
+  .album {
+    height: 64px;
+    width: 64px;
+  }
+  .ml {
+    margin-left: 1rem;
+  }
+  .pointer {
+    cursor: pointer;
+  }
+</style>
