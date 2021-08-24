31697dedfab1695eccad1e346a3a7f390ecb3e5b
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-08-23 22:07:44 -0500

    chore(index.js, stores.js): rename the store variable to playerState

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ import Auth from "./Auth.svelte";
 import SearchBar from "./SearchBar.svelte";
 import TrackSearchResults from "./TrackSearchResults.svelte";
 import { WebPlayback } from "./WebPlayback";
-import { accessToken } from "./stores";
-export { Auth, SearchBar, TrackSearchResults, WebPlayback, accessToken };
+import { playerState } from "./stores";
+export { Auth, SearchBar, TrackSearchResults, WebPlayback, playerState };
